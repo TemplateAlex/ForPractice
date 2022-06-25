6089c2760693fdc5b92815fb393acb0cfdd820a7
Odasaku8 <oaku.ma16@gmail.com>
2022-06-25 12:11:06 +0600

    We added CreateBook Page

---
+++ ForPractice/Pages/CheckBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ForPractice.Pages
+{
+    public class CheckBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
